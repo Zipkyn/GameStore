27f0a575c8f6f1f967bc67dc4b17f6e899a97b5a
Zipkyn <v.t.s.782003@gmail.com>
2025-02-03 18:58:40 +0300

    Настроено подключение PostgreSQL, добавлены миграции и тестовые запросы

--- task1/models.py
+++ task1/models.py
@@ -27,3 +27,10 @@ class News(models.Model):
 
     def __str__(self):
         return self.title
+
+class Store(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
